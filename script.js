0ad839e684a180f0db5199fd36bb3ff64140a592
Sleman37 <sleman0072000@gmail.com>
2025-01-12 21:41:28 +0200

    finished getHumanChoice function
    finished writing the function for asking the player to put his input
    of rock, paper, scissors and it checks if the input by the player is valid

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 let getComputerChoice = () => {
-    value = Math.floor(Math.random() * 3)
+    let value = Math.floor(Math.random() * 3)
     if (value === 0) {
-        return "Rock"
+        return "rock"
     }
     else if (value === 1) {
-        return "Paper"
+        return "paper"
     }
     else {
-        return "Scissors"
+        return "scissors"
     }
 }
 
-console.log(getComputerChoice())
+let getHumanChoice = () => {
+    let playerInput = prompt("Choose: Rock/Paper/Scissors").toLocaleLowerCase()
+
+    if (playerInput === "rock" || playerInput === "paper" || playerInput === "scissors") {
+        return playerInput
+    }
+    else {
+        return "This is not an option in the game"
+    }
+}
+
+console.log(getHumanChoice())
