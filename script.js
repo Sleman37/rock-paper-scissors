4a73be96b5c63731b20b20d7d88521e5d2aca854
Sleman37 <sleman0072000@gmail.com>
2025-01-12 20:57:36 +0200

    finished getComputerChoice function
    finished the function which returns the computer choice of rock, paper, scissors
    which is picked randomly

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+let getComputerChoice = () => {
+    value = Math.floor(Math.random() * 3)
+    if (value === 0) {
+        return "Rock"
+    }
+    else if (value === 1) {
+        return "Paper"
+    }
+    else {
+        return "Scissors"
+    }
+}
+
+console.log(getComputerChoice())
